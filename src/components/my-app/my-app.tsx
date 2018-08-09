4fa162acbe9e24d9faf71df6f01a2ce2bdc25868
paddeee <paddeee@googlemail.com>
2018-08-09 13:13:57 +0100

    Tree structure basic version in place

--- src/components/my-app/my-app.tsx
+++ src/components/my-app/my-app.tsx
@@ -1,44 +1,111 @@
 import '@ionic/core';
-import { Component, Prop, Listen } from '@stencil/core';
+import { Component, State } from '@stencil/core';
+import { Plugins, FilesystemDirectory } from '@capacitor/core';
+
+const { Filesystem } = Plugins;
 
 @Component({
   tag: 'my-app',
   styleUrl: 'my-app.css'
 })
 export class MyApp {
-  @Prop({ connect: 'ion-toast-controller' })
-  toastCtrl: HTMLIonToastControllerElement;
 
-  /**
-   * Handle service worker updates correctly.
-   * This code will show a toast letting the
-   * user of the PWA know that there is a
-   * new version available. When they click the
-   * reload button it then reloads the page
-   * so that the new service worker can take over
-   * and serve the fresh content
-   */
-  @Listen('window:swUpdate')
-  async onSWUpdate() {
-    const toast = await this.toastCtrl.create({
-      message: 'New version available',
-      showCloseButton: true,
-      closeButtonText: 'Reload'
-    });
-    await toast.present();
-    await toast.onWillDismiss();
-    window.location.reload();
+  @State() directoryStructure: any = [
+    { text: "Furniture", type: 'directory', items: [
+        { text: "Sofas", type: 'file', items: [], thumbImage: 'PathOrDataURI.jpg' },
+        { text: "Tables & Chairs", type: 'directory', items: [
+            { text: "Tables.pdf", type: 'file', items: [], thumbImage: 'PathOrDataURI.jpg' },
+            { text: "Chairs.pdf", type: 'file', items: [], thumbImage: 'PathOrDataURI.jpg' }
+          ]
+        },
+        { text: "Occasional Furniture", type: 'file', items: [], thumbImage: 'PathOrDataURI.jpg' }
+      ]
+    },
+    { text: "Kitchen Units.pdf", type: 'file', items: [], thumbImage: 'PathOrDataURI.jpg' },
+    { text: "Decor", type: 'directory', items: [
+        { text: "Bed Linen", type: 'file', items: [], thumbImage: 'PathOrDataURI.jpg' },
+        { text: "Carpets", type: 'file', items: [], thumbImage: 'PathOrDataURI.jpg' },
+        { text: "Curtains & Blinds", type: 'directory', items: [
+            { text: "Curtains.pdf", type: 'file', items: [], thumbImage: 'PathOrDataURI.jpg' },
+            { text: "Blinds.pdf", type: 'file', items: [], thumbImage: 'PathOrDataURI.jpg' }
+          ]
+        }
+      ]
+    }
+  ];
+
+  componentWillLoad() {
+    this.sortDirectories(this.directoryStructure);
+  }
+
+  // Used recursively to drill down through directories to group directories
+  sortDirectories(directories) {
+    directories.sort(this.compareDirectoriesHelper.bind(this))
+  }
+
+  compareDirectoriesHelper(a, b) {
+    if (a.items.length < b.items.length) {
+      this.sortDirectories(b.items);
+      return 1;
+    } else if (a.items.length > b.items.length) {
+      this.sortDirectories(a.items);
+      return -1;
+    }
+    return 0;
+  }
+
+  async readDirectory() {
+    try {
+      const ret = await Filesystem.readdir({
+        path: 'CPSFiles',
+        directory: FilesystemDirectory.Documents
+      });
+      console.log(ret);
+    } catch(e) {
+      console.error('Unable to read dir', e);
+    }
+  }
+
+  createDirectoryTree(directory) {
+    const directoryLength = directory.length;
+
+    if(directoryLength > 0) {
+      return (
+        <ul> {
+          directory.map(subDirectory => {
+            return (
+              <li>
+                {subDirectory.text}
+                {this.createDirectoryTree(subDirectory.items)}
+              </li>
+            )
+          })
+        }
+        </ul>
+      )
+    }
   }
 
   render() {
-    return (
-      <ion-app>
-        <ion-router useHash={false}>
-          <ion-route url="/" component="app-home" />
-          <ion-route url="/profile/:name" component="app-profile" />
-        </ion-router>
-        <ion-nav />
-      </ion-app>
-    );
+    return [
+      <ion-header>
+        <ion-toolbar color="primary">
+          <ion-title>Home</ion-title>
+        </ion-toolbar>
+      </ion-header>,
+      <ion-content>
+        <div class="container">
+          <div class="treeview">
+            <p>
+              <ion-button onClick={() => this.readDirectory()}>Parse Files</ion-button>
+            </p>
+            {this.createDirectoryTree(this.directoryStructure)}
+          </div>
+          <div class="preview-image">
+            <ion-card class="image-card">Preview Image</ion-card>
+          </div>
+        </div>
+      </ion-content>
+    ];
   }
 }
